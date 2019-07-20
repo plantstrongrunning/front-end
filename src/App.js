5cdebcca02bc12d4542471002ee5fdd4ab78e527
Evan Slaton <evanslaton@gmail.com>
2019-07-19 23:14:41 -0700

    Deleted unecessary files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Header from './components/Header';
 import Iin from './components/Iin'
 import Footer from './components/Footer'
-import './App.scss';
 
 function App() {
   return (
