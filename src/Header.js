5890ac7fdee33c46e166cd3682320109127a66e6
Evan Slaton <evanslaton@gmail.com>
2019-07-19 19:18:08 -0700

    Updated/added tags, imported bootstrap and swiper, to the head of index.html

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className="navigation">
+      <img className="img-center" src="img/logo-white-transparent.png" alt="Plant Strong Running logo" />
+    
+      <div className="nav-links">
+        <div id="burger-menu" class="burger-menu-border"><i class="fas fa-bars"></i></div>
+    
+        <ul id="nav-links">
+          <a href="/"><li>Home</li></a>
+          <a href="/about"><li>About</li></a>
+          <a href="/coaching_programs"><li>Coaching Programs</li></a>
+          <a href="/blog"><li>Blog</li></a>
+          <a href="/contact"><li>Contact</li></a>      
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
